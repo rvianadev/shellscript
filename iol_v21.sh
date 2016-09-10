d74c979b82dd51bb077a4a21dc9a87aa474c33e7
Rodrigo Viana <roviol@gmail.com>
2016-09-10 12:36:59 -0300

    Add files via upload

---
+++ iol_v21.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#	Baixa as mensagens do dia do curso online de inglês do IDPH - versão 2.1
+#       Correção do link para download em 13/05/2016
+#	Rodrigo Viana de Oliveira - roviol@gmail.com
+#
+
+ano=`date +%Y`
+hoje=`date +%j`
+
+for n in `seq -w 1 $hoje`
+do
+    arquivo=$ano$n
+    if [ ! -e /home/rodrigo/Documentos/IOL/Mensagem\ do\ dia/$arquivo.mp3 ]
+    then
+        dir=`echo $arquivo | cut -c5-7`
+        wget -c -t5 -P /home/rodrigo/Documentos/IOL/Mensagem\ do\ dia  http://www.aprendendoingles.com.br/mp3/$arquivo.mp3 &&
+        echo "*** O arquivo $arquivo.mp3 foi baixado com êxito em `date`." >> /home/rodrigo/Documentos/IOL/Mensagem\ do\ dia/iol.log
+    fi
+done
